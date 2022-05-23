34b57a49774825d38f7429316c3727666962f14e
변성도 <dmdsns@naver.com>
2022-05-23 10:28:22 +0900

    평점 별 표시

--- movies/forms.py
+++ movies/forms.py
@@ -2,15 +2,32 @@ from django import forms
 from movies.models import MovieComment
 
 class MovieCommentForm(forms.ModelForm):
-    rank = forms.IntegerField(
-        widget=forms.Textarea(
-            attrs={
-                'rows' : 1,
-                'cols' : 1
-            }
+    # rank = forms.IntegerField(
+    #     widget=forms.Textarea(
+    #         attrs={
+    #             'rows' : 1,
+    #             'cols' : 1
+    #         }
             
-        )
-    )
+    #     )
+    # )
+
+    rank_one = 1
+    rank_two = 2
+    rank_three = 3
+    rank_four =  4
+    rank_five = 5
+
+    rank_choice = [
+        (rank_one, '1'),
+        (rank_two, '2'),
+        (rank_three, '3'),
+        (rank_four, '4'),
+        (rank_five, '5'),
+    ]
+
+    rank = forms.ChoiceField(choices=rank_choice, widget=forms.Select())
+
     content = forms.CharField(
         label='',
         widget=forms.Textarea(
