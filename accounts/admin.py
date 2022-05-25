6c0700f521a14acd2cda549d3096b88f9ed19661
진주현 <jinju0405@naver.com>
2022-05-25 16:48:48 +0900

    movie/index 디자인

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
+from community.models import Article
+from movies.models import Movie
+from .models import User
 
 # Register your models here.
+class ArticleAdmin(admin.ModelAdmin):
+	list_display=('pk', 'title', 'content', 'created_at', 'updated_at',)
+
+class MovieAdmin(admin.ModelAdmin):
+	list_display="__all__"
+
+class UserAdmin(admin.ModelAdmin):
+	list_display="__all__"
+
+admin.site.register(Article)
+admin.site.register(Movie)
+admin.site.register(User)
